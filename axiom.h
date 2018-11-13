79756289423a325baa9e12b85498706334ed66ab
amlord <tcc7130@gmail.com>
2018-11-12 20:20:29 -0800

    sin syntax

--- axiom.h
+++ axiom.h
@@ -1,9 +1,10 @@
 typedef enum {	VARIABLE_NAME, VARIABLE_TYPE, KEYWORD_WHILE, KEYWORD_FOR, KEYWORD_IF, KEYWORD_ELSE,	KEYWORD_ELIF, 				KEYWORD_READ, KEYWORD_PRINT, KEYWORD_PRINTLN, INTEGER, ARITMETIC_OP, COMPARE_OP, ASSIGN, INCREMENT,	 			DECREMENT, PAREN_L, PAREN_R, CORCHETE_L, CORCHETE_R, LLAVE_L, LLAVE_R, SEMICOLON, COMILLA_DOB,					COMILLA_SIN, COMA, STRING, DECIMAL, KCHAR, OPERAND } TokenType;
 
 struct token{
-	int id, line;
+	int id;
+	int line;
 	char *content;
-	struct token *next, *papa;
+	struct token *next;
 };
 
 struct tokenList{
@@ -16,7 +17,7 @@ struct token *checksToken(char *word, struct token *tok);
 //struct token *createToken(int id, char *word, struct token *tok, int papa);
 struct token *createToken(int id, char *word, struct token *tok);
 struct tokenList *lex(FILE *fp);
-void syntax(struct tokenList *tokens);
+//void syntax(struct tokenList *tokens);
 
 struct token *Expression(struct token *tk);
 struct token *Condition(struct token *tk);
